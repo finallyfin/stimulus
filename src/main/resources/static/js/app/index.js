b28fdc353046dc61a2bf3e6e2ec329b7e81297bc
finallyfin <churn82@nate.com>
2022-03-02 22:41:03 +0900

    수정기능완료

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,15 +1,15 @@
-var main = {
-    init: function (){
-        var _this = this;
-        $('#btn-save').on('click', function (){
+const main = {
+    init: function () {
+        const _this = this;
+        $('#btn-save').on('click', function () {
             _this.save();
         });
-        $('#btn-update').on('click', function (){
+        $('#btn-update').on('click', function () {
             _this.update();
         });
     },
-    save : function() {
-        var data = {
+    save: function () {
+        const data = {
             title: $('#title').val(),
             author: $('#author').val(),
             content: $('#content').val()
@@ -20,31 +20,31 @@ var main = {
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function(){
+        }).done(function () {
             alert('글이 등록되었습니다.');
-            window.locaiton.href = '/';
-        }).fail(function (error){
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-    update : function() {
-        var data = {
+    update: function () {
+        const data = {
             title: $('#title').val(),
             content: $('#content').val()
         };
 
-        var id = $('#id').val();
+        const id = $('#id').val();
 
         $.ajax({
             type: 'PUT', //controller에서 @putmapping을 했기 때문에 put을 사용해야한다.
-            url: '/api/v1/posts/'+id,
+            url: '/api/v1/posts/' + id,
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function(){
+        }).done(function () {
             alert('글이 수정되었습니다.');
-            window.locaiton.href = '/';
-        }).fail(function (error){
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
 
